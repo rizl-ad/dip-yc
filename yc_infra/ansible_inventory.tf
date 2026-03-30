d2a618423d42ab48435352566f38299393503a07
rizl-ad <rizl@list.ru>
2026-03-30 20:24:12 +0300

    bug fix

--- yc_infra/ansible_inventory.tf
+++ yc_infra/ansible_inventory.tf
@@ -14,7 +14,7 @@ module "ansible_inventory_file" {
   content = templatefile(
     "${path.root}/tftpl/inventory.tftpl", {
       global_vars = {
-        user_name                   = var.vm_user_name
+        user_name = var.vm_user_name
         # ssh_key_path                = var.ssh_key_path
         connection_type             = var.ansible_inventory.connection_type
         apiserver_advertise_address = local.k8s_first_master_ip
