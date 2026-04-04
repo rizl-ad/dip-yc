2bfc9f99c378ecb1ede65bd3f1a8260c666aa7a5
rizl-ad <rizl@list.ru>
2026-04-04 23:14:57 +0300

    update tf

--- yc_infra/app_data.tf
+++ yc_infra/app_data.tf
@@ -3,7 +3,7 @@ module "app_data_file" {
   bucket_name = var.bucket.name
   content = jsonencode({
     registry_id = module.container_registry.id
-    port = var.k8s_service_lb.listener_port
+    port        = var.k8s_service_lb.listener_port
   })
   content_type = var.app_data.content_type
   object_key   = "${var.app_data.dir_path}/${var.app_data.name}"
